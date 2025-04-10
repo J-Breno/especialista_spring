023fbe5b19e89e4b6ed3b9fa1c70eaf152e44180
J-Breno <joao.breno85@hotmail.com>
2025-04-10 10:20:20 -0300

    aprendendo dependencia opcional com o @Autowired

--- 01-injeção_de_dependencias/algafood-api/src/main/java/com/github/jbreno/algafood/di/service/ActivationClientService.java
+++ 01-injeção_de_dependencias/algafood-api/src/main/java/com/github/jbreno/algafood/di/service/ActivationClientService.java
@@ -1,20 +1,32 @@
 package com.github.jbreno.algafood.di.service;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.github.jbreno.algafood.di.modelo.Client;
 import com.github.jbreno.algafood.di.notification.Notifier;
 
 
 public class ActivationClientService {
+	
+	@Autowired(required = false)
 	private Notifier notifier;
 	
-	public ActivationClientService(Notifier notifier) {
-		this.notifier = notifier;
-	}
+//	@Autowired
+//	public ActivationClientService(Notifier notifier) {
+//		this.notifier = notifier;
+//	}
+	
+//	@Autowired
+//	public void setNotifier(Notifier notifier) {
+//		this.notifier = notifier;
+//	}
 
 	public void active(Client client) {
 		client.active();
+		if(notifier != null) {
 		notifier.notify(client, "Seu cadastro no sistema está ativo!");
+		} else {
+			System.out.println("Não existe notificador, mas cliente foi ativado");
+		}
 	}
 }

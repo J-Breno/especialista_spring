3f7a9fd69a995142d03f6aa1b84d5e7bfcd9d806
J-Breno <joao.breno85@hotmail.com>
2025-04-11 02:15:30 -0300

    mapeando entidades com JPA

---
+++ 02-introdução_jpa_hibernate/algafood-api/src/main/java/com/github/jbreno/algafood/domain/model/Kitchen.java
@@ -0,0 +1,10 @@
+package com.github.jbreno.algafood.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_kitchen")
+public class Kitchen {
+
+}

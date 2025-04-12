b0ea107d20f2031c1018e0ac533ee74e315a9ea0
J-Breno <joao.breno85@hotmail.com>
2025-04-11 21:05:42 -0300

    propriedade nullable de @Column e @JoinColumn

--- 02-introdução_jpa_hibernate/algafood-api/src/main/java/com/github/jbreno/algafood/domain/model/Restaurant.java
+++ 02-introdução_jpa_hibernate/algafood-api/src/main/java/com/github/jbreno/algafood/domain/model/Restaurant.java
@@ -2,6 +2,7 @@ package com.github.jbreno.algafood.domain.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -23,10 +24,12 @@ public class Restaurant {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
 	private Long id;
+	@Column(nullable = false)
 	private String name;
+	@Column(nullable = false)
 	private BigDecimal shippingFee;
 	
 	@ManyToOne
-	@JoinColumn(name = "kitchen_id")
+	@JoinColumn(name = "kitchen_id", nullable = false)
 	private Kitchen kitchen;
 }

391a6582c73431e0159ba7662bd653003cbd307f
J-Breno <joao.breno85@hotmail.com>
2025-04-12 16:59:43 -0300

    Consultando Singleton Resource com GET e @PathVariable

--- 02-introdução_jpa_hibernate/algafood-api/src/main/java/com/github/jbreno/algafood/api/controller/KitchenController.java
+++ 02-introdução_jpa_hibernate/algafood-api/src/main/java/com/github/jbreno/algafood/api/controller/KitchenController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import  org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,11 @@ public class KitchenController {
 	@GetMapping
 	public List<Kitchen> list() {
 		return kitchenRepository.all();
+	}
+	
+	@GetMapping("/{id}")
+	public Kitchen search(@PathVariable Long id) {
+		return kitchenRepository.search(id);
 	}
 
 }

21181ecd66a48a1cda9c1b221a5473ccf912f63b
J-Breno <joao.breno85@hotmail.com>
2025-04-13 02:31:33 -0300

    Customizando a representação em XML com Wrapper e anotações do Jackson

--- 02-introdução_jpa_hibernate/algafood-api/src/main/java/com/github/jbreno/algafood/api/controller/KitchenController.java
+++ 02-introdução_jpa_hibernate/algafood-api/src/main/java/com/github/jbreno/algafood/api/controller/KitchenController.java
@@ -3,17 +3,19 @@ package com.github.jbreno.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import  org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.jbreno.algafood.api.model.KitchensXmlWrapper;
 import com.github.jbreno.algafood.domain.model.Kitchen;
 import com.github.jbreno.algafood.domain.repository.KitchenRepository;
 
+
 @RestController
-@RequestMapping(value = "/kitchens", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+@RequestMapping(value = "/kitchens")
 public class KitchenController {
 	
 	@Autowired
@@ -24,6 +26,11 @@ public class KitchenController {
 		return kitchenRepository.all();
 	}
 	
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public KitchensXmlWrapper listXml() {
+		return new KitchensXmlWrapper(kitchenRepository.all());
+	}
+
 	@GetMapping("/{id}")
 	public Kitchen search(@PathVariable Long id) {
 		return kitchenRepository.search(id);
--- 02-introdução_jpa_hibernate/algafood-api/src/main/java/com/github/jbreno/algafood/domain/model/Kitchen.java
+++ 02-introdução_jpa_hibernate/algafood-api/src/main/java/com/github/jbreno/algafood/domain/model/Kitchen.java
@@ -7,16 +7,19 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@JsonRootName("cozinha")
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "tb_kitchen")
 public class Kitchen {
 	
-	@Id
+	@Id	
 	@EqualsAndHashCode.Include
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;

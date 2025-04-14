b196901ab18c3d512392b3191ecfa800c9809473
J-Breno <joao.breno85@hotmail.com>
2025-04-14 14:46:03 -0300

    POST não é indepotente. Modelando e Implementando a inclusão de recursos com POST

--- 02-introdução_jpa_hibernate/algafood-api/src/main/java/com/github/jbreno/algafood/api/controller/KitchenController.java
+++ 02-introdução_jpa_hibernate/algafood-api/src/main/java/com/github/jbreno/algafood/api/controller/KitchenController.java
@@ -8,7 +8,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.jbreno.algafood.api.model.KitchensXmlWrapper;
@@ -43,5 +46,10 @@ public class KitchenController {
 //		return ResponseEntity.status(HttpStatus.OK).body(kitchen);
 		return ResponseEntity.notFound().build();
 	}
-
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Kitchen adcionar(@RequestBody Kitchen kitchen) {
+		return kitchenRepository.save(kitchen);
+	}
 }

0902dca13d4071ba73364cc26be442a593c645ac
J-Breno <joao.breno85@hotmail.com>
2025-04-16 15:02:33 -0300

    modelando e implementando a inclusão de recursos de restaurantes

--- 02-introdução_jpa_hibernate/algafood-api/src/main/java/com/github/jbreno/algafood/api/controller/RestaurantController.java
+++ 02-introdução_jpa_hibernate/algafood-api/src/main/java/com/github/jbreno/algafood/api/controller/RestaurantController.java
@@ -3,11 +3,17 @@ package com.github.jbreno.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.jbreno.algafood.domain.exception.EntityNotFoundException;
 import com.github.jbreno.algafood.domain.model.Restaurant;
 import com.github.jbreno.algafood.domain.service.RestaurantRegistrationService;
 
@@ -24,7 +30,23 @@ public class RestaurantController {
 	}
 	
 	@GetMapping("/{id}")
-	public Restaurant search(@PathVariable Long id) {
-		return restaurantService.search(id);
+	public ResponseEntity<Restaurant> search(@PathVariable Long id) {
+		Restaurant restaurant = restaurantService.search(id);
+		
+		if(restaurant != null) {
+			return ResponseEntity.ok(restaurant);
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> add(@RequestBody Restaurant restaurant) {
+		try {
+		  restaurant = restaurantService.save(restaurant);
+		  return ResponseEntity.status(HttpStatus.CREATED)
+				  .body(restaurant);
+		} catch(EntityNotFoundException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
 }

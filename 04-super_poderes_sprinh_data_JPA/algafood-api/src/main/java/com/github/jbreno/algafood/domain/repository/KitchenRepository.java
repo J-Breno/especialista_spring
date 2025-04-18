0aa116f00842471aa4180b142c8cc8ecdc09e5c3
J-Breno <joao.breno85@hotmail.com>
2025-04-18 10:58:31 -0300

    conhecendo os prefixos de query methods

--- 04-super_poderes_sprinh_data_JPA/algafood-api/src/main/java/com/github/jbreno/algafood/domain/repository/KitchenRepository.java
+++ 04-super_poderes_sprinh_data_JPA/algafood-api/src/main/java/com/github/jbreno/algafood/domain/repository/KitchenRepository.java
@@ -1,6 +1,7 @@
 package com.github.jbreno.algafood.domain.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,11 @@ import com.github.jbreno.algafood.domain.model.Kitchen;
 
 @Repository
 public interface KitchenRepository extends JpaRepository<Kitchen, Long>{
-	List<Kitchen> searhByName(String name);
+	List<Kitchen> findAllByName(String name);
+	
+	Optional<Kitchen> findByName(String name);	
+	
+	boolean existsByName(String name);
+	
 	
 }
--- 04-super_poderes_sprinh_data_JPA/algafood-api/src/main/java/com/github/jbreno/algafood/domain/repository/RestaurantRepository.java
+++ 04-super_poderes_sprinh_data_JPA/algafood-api/src/main/java/com/github/jbreno/algafood/domain/repository/RestaurantRepository.java
@@ -1,8 +1,21 @@
 package com.github.jbreno.algafood.domain.repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.github.jbreno.algafood.domain.model.Restaurant;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long>{
+		List<Restaurant> findByShippingFeeBetween(BigDecimal initialFee, BigDecimal finalFee);
+		
+		List<Restaurant> findByNameContainingAndKitchenId(String name, Long kitchen);
+		
+		Optional<Restaurant> findFirstRestaurantByNameContaining(String name);
+		
+		List<Restaurant> findTop2ByNameContaining(String name);
+		
+		int countByKitchenId(Long id);
 }

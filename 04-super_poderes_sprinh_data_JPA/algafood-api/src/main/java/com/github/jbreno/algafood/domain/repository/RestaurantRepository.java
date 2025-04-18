a74df0f0b9ea68f94ef5b84f781c97fd576d5b97
J-Breno <joao.breno85@hotmail.com>
2025-04-18 11:34:06 -0300

    Usando queries JPQL customizadas com @Query

--- 04-super_poderes_sprinh_data_JPA/algafood-api/src/main/java/com/github/jbreno/algafood/domain/repository/RestaurantRepository.java
+++ 04-super_poderes_sprinh_data_JPA/algafood-api/src/main/java/com/github/jbreno/algafood/domain/repository/RestaurantRepository.java
@@ -5,13 +5,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.github.jbreno.algafood.domain.model.Restaurant;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long>{
 		List<Restaurant> findByShippingFeeBetween(BigDecimal initialFee, BigDecimal finalFee);
-		
-		List<Restaurant> findByNameContainingAndKitchenId(String name, Long kitchen);
+
+		@Query("FROM Restaurant WHERE name LIKE %:name% AND kitchen.id = :id")
+		List<Restaurant> searchByName(String name, @Param("id") Long kitchen);
+//		List<Restaurant> findByNameContainingAndKitchenId(String name, Long kitchen);
 		
 		Optional<Restaurant> findFirstRestaurantByNameContaining(String name);
 		

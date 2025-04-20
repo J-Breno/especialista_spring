0a9459993be18235d26b6eb14f673ccf63f87365
J-Breno <joao.breno85@hotmail.com>
2025-04-20 12:40:11 -0300

    Desafio mapeando relacionamento muitos-para-um

---
+++ 04-super_poderes_sprinh_data_JPA/algafood-api/src/main/java/com/github/jbreno/algafood/domain/model/Product.java
@@ -0,0 +1,44 @@
+package com.github.jbreno.algafood.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "tb_product")
+public class Product {
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private String description;
+	
+	@Column(nullable = false)
+	private BigDecimal price;
+	
+	@Column(nullable = false)
+	private Boolean active;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Restaurant restaurant;
+	
+}

445a1536f163382dbf716b53b0d728e73a16ca0d
J-Breno <joao.breno85@hotmail.com>
2025-04-23 03:40:54 -0300

    Estendendo o Problem Details para adicionar as propriedades com constraints violdas

--- 06-validacoes_bean_validation/algafood-api/src/main/java/com/github/jbreno/algafood/api/exceptionhandler/Problem.java
+++ 06-validacoes_bean_validation/algafood-api/src/main/java/com/github/jbreno/algafood/api/exceptionhandler/Problem.java
@@ -1,6 +1,7 @@
 package com.github.jbreno.algafood.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -17,4 +18,12 @@ public class Problem {
 	
 	private String userMessage;
 	
+	private List<Field> fields;
+	
+	@Getter
+	@Builder
+	public static class Field {
+		private String name;
+		private String userMessage;
+	}
 }

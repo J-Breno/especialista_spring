64a71f60114a29ca9912c092e247b77c335d36cc
J-Breno <joao.breno85@hotmail.com>
2025-04-24 14:54:16 -0300

    Implementando Testes de API com REST Assured e validando o código de status HTTP

--- 07-teste_de_integracao/algafood-api/src/test/java/com/github/jbreno/algafood/KitchenRegistrationIT.java
+++ 07-teste_de_integracao/algafood-api/src/test/java/com/github/jbreno/algafood/KitchenRegistrationIT.java
@@ -1,46 +1,30 @@
 package com.github.jbreno.algafood;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.github.jbreno.algafood.domain.exception.EntityInUseException;
-import com.github.jbreno.algafood.domain.exception.KitchenNotFoundException;
-import com.github.jbreno.algafood.domain.model.Kitchen;
-import com.github.jbreno.algafood.domain.service.KitchenRegistrationService;
+import static io.restassured.RestAssured.given;
+import io.restassured.http.ContentType;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class KitchenRegistrationIT {
 	
-	@Autowired
-	private KitchenRegistrationService kitchenService;
-
-	@Test
-	public void shouldKitchenRegistrationSuccessfully_WhenKitchenCorrect() {
-		// cenário
-		Kitchen newKitchen = new Kitchen();
-		newKitchen.setName("Chinesa");
-		
-		// ação
-		newKitchen = kitchenService.save(newKitchen);
-		
-		// validação
-		assertThat(newKitchen).isNotNull();
-		assertThat(newKitchen.getId()).isNotNull();
-	}
-
-	@Test(expected = EntityInUseException.class)
-	public void shouldFail_WhenDeleteKitchenInUse() {	
-		kitchenService.remove(1L);
-	}
+	@LocalServerPort
+	private int port;
 	
-	@Test(expected = KitchenNotFoundException.class)
-	public void shoudFail_WhenKitchenDoesNotExist() {
-		kitchenService.remove(100L);
+	@Test
+	public void shouldReturnStatus200_WhenConsultKitchens() {
+	given()
+		.basePath("/kitchens")
+		.port(port)
+		.accept(ContentType.JSON)
+	.when()
+		.get()
+	.then()
+		.statusCode(HttpStatus.OK.value());
 	}
 }

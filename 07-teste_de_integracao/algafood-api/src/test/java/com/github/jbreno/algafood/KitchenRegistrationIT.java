0f9e885313bcc57e2e174b4e9bb33d62e479f698
J-Breno <joao.breno85@hotmail.com>
2025-04-25 00:22:42 -0300

    Criando um método para fazer setup dos testes

--- 07-teste_de_integracao/algafood-api/src/test/java/com/github/jbreno/algafood/KitchenRegistrationIT.java
+++ 07-teste_de_integracao/algafood-api/src/test/java/com/github/jbreno/algafood/KitchenRegistrationIT.java
@@ -4,6 +4,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @RunWith(SpringRunner.class)
@@ -20,11 +22,16 @@ public class KitchenRegistrationIT {
 	@LocalServerPort
 	private int port;
 	
+	@Before
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/kitchens";
+	}
+	
 	@Test
 	public void shouldReturnStatus200_WhenConsultKitchens() {
 	given()
-		.basePath("/kitchens")
-		.port(port)
 		.accept(ContentType.JSON)
 	.when()
 		.get()
@@ -36,8 +43,6 @@ public class KitchenRegistrationIT {
 	public void shouldContain2Kitchen_WhenConsultKitchens() {
 		
 	given()
-		.basePath("/kitchens")
-		.port(port)
 		.accept(ContentType.JSON)
 	.when()
 		.get()

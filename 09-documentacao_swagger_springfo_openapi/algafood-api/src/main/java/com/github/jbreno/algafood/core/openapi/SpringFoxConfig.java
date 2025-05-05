f86ea836f55ba9262bcae63d4b4d78a53d1e04bf
J-Breno <joao.breno85@hotmail.com>
2025-05-05 03:22:48 -0300

    Corrigindo documentação com substituição de Pageable

--- 09-documentacao_swagger_springfo_openapi/algafood-api/src/main/java/com/github/jbreno/algafood/core/openapi/SpringFoxConfig.java
+++ 09-documentacao_swagger_springfo_openapi/algafood-api/src/main/java/com/github/jbreno/algafood/core/openapi/SpringFoxConfig.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -13,6 +15,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.fasterxml.classmate.TypeResolver;
 import com.github.jbreno.algafood.api.exceptionhandler.Problem;
+import com.github.jbreno.algafood.core.openapi.model.PageableDtoOpenApi;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -48,6 +51,7 @@ public class SpringFoxConfig implements WebMvcConfigurer{
 				.globalResponseMessage(RequestMethod.PUT, globalPostPutResponseMessages())
 				.globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())
 				.additionalModels(typeResolver.resolve(Problem.class))
+				.directModelSubstitute(Pageable.class, PageableDtoOpenApi.class)
 				.apiInfo(apiInfo())
 				.tags(
 						new Tag("Cidades", "Gerencia as cidades"),
